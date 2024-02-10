addc8cf3fd560785b941916176f2f01240e25eff
Sri01729 <salahari@northislandcollege.ca>
2024-02-09 23:14:50 -0800

    add login form

--- DHVANI/views/login.view.php
+++ DHVANI/views/login.view.php
@@ -13,14 +13,45 @@
 
 <body>
     <header>
-        <nav class="topnav" id="myTopnav">
-            <a href="#signup">Signup</a>
-            <a href="#login" class="active">Login</a>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-            </a>
+        <nav class="navbar">
+            <a href="/" class="logo">DHVANI</a>
+            <div class="nav-buttons">
+                <button onclick="location.href='/login'" class="btn login">Login</button>
+                <button onclick="location.href='/signup'" class="btn signup">Signup</button>
+            </div>
         </nav>
     </header>
+    <main>
+
+        <div class="form-box">
+            <div class="form-value">
+                <form action="">
+                    <h2>Login</h2>
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="email" required>
+                        <label for="">Email</label>
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password" required>
+                        <label for="">Password</label>
+                    </div>
+                    <div class="forget">
+                        <label><input type="checkbox">Remember Me</label>
+                        <a href="#">Forget password?</a>
+
+                    </div>
+                    <button>Log in</button>
+                    <div class="register">
+                        <p>Don't have an account ? </p>
+                        <a href="../DGL_123_PROJECT/Controllers/signup.php"> Register</a>
+                </form>
+            </div>
+        </div>
+    </main>
+
+
 
     <script src="/DHVANI/assets/js/login.js" async defer></script>
 </body>
