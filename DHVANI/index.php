5feaaeacbd9f2b773744cd3d1e9ccd6d758ffd63
Sri01729 <salahari@northislandcollege.ca>
2024-02-09 22:02:06 -0800

    Create views and add html boilerplate in login view

---
+++ DHVANI/index.php
@@ -0,0 +1,8 @@
+<?php
+// Include the header
+include 'views/login.view.php';
+
+
+
+// Include the footer/
+include 'views/footer.view.php';
---
+++ DHVANI/views/home.view.php
@@ -0,0 +1,13 @@
+<div class="wrapper">
+    <!--sidebar -->
+    <aside id="sidebar">
+        <div class="h-100">
+            <div class="sidebar-logo">
+                <a href="#">DHVANI</a>
+            </div>
+        </div>
+    </aside>
+
+
+
+</div>
