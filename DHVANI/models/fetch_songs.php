16cd621f9010d619c075d19c3ecb0c2af8937fd2
Sri01729 <salahari@northislandcollege.ca>
2024-02-15 02:03:32 -0800

    Navigate through songs upon button actions

--- DHVANI/models/fetch_songs.php
+++ DHVANI/models/fetch_songs.php
@@ -7,6 +7,17 @@ require '../Core/databaseconnection.php'; // Include the database connection cod
 // Initialize or update the song index
 if (!isset($_SESSION['songIndex'])) {
     $_SESSION['songIndex'] = 0; // Start with the first song
+} else {
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'next') {
+            $_SESSION['songIndex']++;
+        } elseif ($_GET['action'] == 'prev') {
+            $_SESSION['songIndex']--;
+            if ($_SESSION['songIndex'] < 0) {
+                $_SESSION['songIndex'] = 0; // Prevent going below 0
+            }
+        }
+    }
 }
 
 
