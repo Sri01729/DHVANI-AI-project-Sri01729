6889effd7ef2d660f27b177ad40a3dbbebf3fd1e
Sri01729 <salahari@northislandcollege.ca>
2024-02-15 02:01:19 -0800

    Initialize the song index value with zero

--- DHVANI/models/fetch_songs.php
+++ DHVANI/models/fetch_songs.php
@@ -2,3 +2,12 @@
 // Start a session
 session_start();
 require '../Core/databaseconnection.php'; // Include the database connection code
+
+
+// Initialize or update the song index
+if (!isset($_SESSION['songIndex'])) {
+    $_SESSION['songIndex'] = 0; // Start with the first song
+}
+
+
+$query = $pdo->prepare("");
