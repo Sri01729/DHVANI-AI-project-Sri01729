f2304ca1aa22c3cf2109aa667d0091210fc98c0b
Sri01729 <salahari@northislandcollege.ca>
2024-04-05 03:09:48 -0700

    Create about.php page

---
+++ DHVANI/controllers/about.php
@@ -0,0 +1,10 @@
+<?php
+// Include the header
+include '../views/header.view.php';
+
+// Include the home page
+include '../views/about.view.php';
+
+
+//Include the footer
+include '../views/footer.view.php';
