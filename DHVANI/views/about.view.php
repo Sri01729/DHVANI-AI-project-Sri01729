1e24c5704e8376a958ec6653088de16b88686397
Sri01729 <salahari@northislandcollege.ca>
2024-04-05 02:43:08 -0700

    Add 'Who We Are' section to About Us page, outlining Dhvani's community and vision.

---
+++ DHVANI/views/about.view.php
@@ -0,0 +1,16 @@
+<div class="content">
+    <section class="content-section">
+        <div class="container">
+            <article class="content-block">
+                <h2 class="heading">Who We Are</h2>
+                <p class="text">Dhvani, a revolutionary music streaming service designed to provide you with
+                    personalized soundtracks that match your mood, weather, and location through the power of AI. I've
+                    built Dhvani to be more than just a service; it's a testament to the transcendent power of music and
+                    its ability to resonate with our innermost emotions. For me, Dhvani is a community where every note
+                    plays a part in the symphony of life.</p>
+            </article>
+
+
+        </div>
+    </section>
+</div>
