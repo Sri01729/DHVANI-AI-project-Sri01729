08c6ab0859bb65c0bc2f6629377945c9873b880b
Sri01729 <salahari@northislandcollege.ca>
2024-04-05 02:53:47 -0700

    Add copyright text in footer

--- DHVANI/views/footer.view.php
+++ DHVANI/views/footer.view.php
@@ -1,3 +1,8 @@
 </main>
+<footer>
+    <div class="container">
+        <p>&copy; 2024 Dhvani Music. All Rights Reserved.</p>
+    </div>
+</footer>
 <script type="module" src="/DHVANI/assets/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/intro.js/minified/intro.min.js"></script>
